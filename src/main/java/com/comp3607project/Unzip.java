454ec0f249425421c226217ad8310f873df4ed6b
D-e-v-M <deveshmotie@gmail.com>
2023-11-07 19:28:46 -0400

    Added package line to Unzip class

--- src/main/java/com/comp3607project/Unzip.java
+++ src/main/java/com/comp3607project/Unzip.java
@@ -1,3 +1,5 @@
+package com.comp3607project;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.util.*;
@@ -7,39 +9,33 @@ import java.util.zip.ZipEntry;
 
 import java.util.zip.ZipFile;
 
+public class Unzip {
 
-public class Unzip  {
-    
-    public Unzip(String filename) throws IOException
-    {
-        ArrayList<String> names=new ArrayList<>();
-        unzipFile(filename,names);
-        unZipNested(names);
-    }
-    public static void unzipFile(String filename, ArrayList<String> names) throws IOException{
+  public Unzip(String filename) throws IOException {
+    ArrayList<String> names = new ArrayList<>();
+    unzipFile(filename, names);
+    unZipNested(names);
+  }
+
+  public static void unzipFile(String filename, ArrayList<String> names) throws IOException {
     String fileZip = filename;
     File destDir = new File("src");
 
-    try (ZipFile file = new ZipFile(fileZip))
-    {
+    try (ZipFile file = new ZipFile(fileZip)) {
       Enumeration<? extends ZipEntry> zipEntries = file.entries();
-      while (zipEntries.hasMoreElements())
-      {
+      while (zipEntries.hasMoreElements()) {
         ZipEntry zipEntry = zipEntries.nextElement();
         File newFile = new File(destDir, zipEntry.getName());
-        names.add("src\\"+zipEntry.getName());
-        //create sub directories
+        names.add("src\\" + zipEntry.getName());
+        // create sub directories
         newFile.getParentFile().mkdirs();
 
-        if (!zipEntry.isDirectory())
-        {
-          try (FileOutputStream outputStream = new FileOutputStream(newFile))
-          {
+        if (!zipEntry.isDirectory()) {
+          try (FileOutputStream outputStream = new FileOutputStream(newFile)) {
             BufferedInputStream inputStream = new BufferedInputStream(file.getInputStream(zipEntry));
-            while (inputStream.available() > 0)
-            {
-        
-outputStream.write(inputStream.read());
+            while (inputStream.available() > 0) {
+
+              outputStream.write(inputStream.read());
             }
             inputStream.close();
           }
@@ -47,41 +43,37 @@ outputStream.write(inputStream.read());
 
       }
     }
-    
-}
-public static void unZipNested(ArrayList<String> names) throws IOException{
-    Iterator<String> iter  = names.iterator(); 
-    while (iter.hasNext()) { 
-             String fileZip = iter.next();
-    File destDir = new File("src");
 
-    try (ZipFile file = new ZipFile(fileZip))
-    {
-      Enumeration<? extends ZipEntry> zipEntries = file.entries();
-      while (zipEntries.hasMoreElements())
-      {
-        ZipEntry zipEntry = zipEntries.nextElement();
-        File newFile = new File(destDir, zipEntry.getName());
-       
-        //create sub directories
-        newFile.getParentFile().mkdirs();
+  }
 
-        if (!zipEntry.isDirectory())
-        {
-          try (FileOutputStream outputStream = new FileOutputStream(newFile))
-          {
-            BufferedInputStream inputStream = new BufferedInputStream(file.getInputStream(zipEntry));
-            while (inputStream.available() > 0)
-            {
-        
-outputStream.write(inputStream.read());
+  public static void unZipNested(ArrayList<String> names) throws IOException {
+    Iterator<String> iter = names.iterator();
+    while (iter.hasNext()) {
+      String fileZip = iter.next();
+      File destDir = new File("src");
+
+      try (ZipFile file = new ZipFile(fileZip)) {
+        Enumeration<? extends ZipEntry> zipEntries = file.entries();
+        while (zipEntries.hasMoreElements()) {
+          ZipEntry zipEntry = zipEntries.nextElement();
+          File newFile = new File(destDir, zipEntry.getName());
+
+          // create sub directories
+          newFile.getParentFile().mkdirs();
+
+          if (!zipEntry.isDirectory()) {
+            try (FileOutputStream outputStream = new FileOutputStream(newFile)) {
+              BufferedInputStream inputStream = new BufferedInputStream(file.getInputStream(zipEntry));
+              while (inputStream.available() > 0) {
+
+                outputStream.write(inputStream.read());
+              }
+              inputStream.close();
             }
-            inputStream.close();
           }
-        }
 
+        }
       }
     }
-        } 
-}
+  }
 }
