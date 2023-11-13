12d7510aa6c4fb6ff66bd4948aafeeb1ebde7c24
NJaganath <101383986+NJaganath@users.noreply.github.com>
2023-11-13 11:32:50 -0500

    Update PassengerTest.java

--- src/main/java/com/comp3607project/PassengerTest.java
+++ src/main/java/com/comp3607project/PassengerTest.java
@@ -1,77 +1,109 @@
 package com.comp3607project;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PassengerTest {
-    private Passenger passenger;
 
+    String passportNumber = "TA890789";
+    String firstName = "Joe";
+    String lastName = "Bean";
+    String flightNo = "BW600";
+
+    private Passenger passenger;
+    private static int marksAwarded = 0;
+    
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         // Create a Passenger instance before each test
-        passenger = new Passenger("TA890789", "Joe", "Bean", "FL123");
+        passenger = new Passenger("TA890789", "Joe", "Bean", "BW600");
         passenger.assignRandomCabinClass();
         passenger.assignRandomNumLuggage();
     }
 
     @Test
-    void testPassportNumber() {
+    public void testPassportNumberAttribute(){
         // Test the passportNumber attribute type
         assertNotNull(passenger.getPassportNumber());
         assertTrue(passenger.getPassportNumber() instanceof String);
+        marksAwarded = marksAwarded + 1;
     }
 
     @Test
-    void testFlightNo() {
+    public void testFlightNoAttribute() {
         // Test the flightNo attribute type
         assertNotNull(passenger.getFlightNo());
         assertTrue(passenger.getFlightNo() instanceof String);
+        marksAwarded = marksAwarded + 1;
     }
 
     @Test
-    void testFirstName() {
+    public void testFirstNameAttribute() {
         // Test the firstName attribute type
         assertNotNull(passenger.getFirstName());
         assertTrue(passenger.getFirstName() instanceof String);
+        marksAwarded = marksAwarded + 1;
     }
 
     @Test
-    void testLastName() {
+    public void testLastNameAttribute() {
         // Test the lastName attribute type
         assertNotNull(passenger.getLastName());
         assertTrue(passenger.getLastName() instanceof String);
+        marksAwarded = marksAwarded + 1;
     }
 
     @Test
-    void testNumLuggage() {
+    public void testNumLuggageAttribute() {
         // Test the numLuggage attribute type
-        assertTrue(passenger.getNumLuggage() >= 1 && passenger.getNumLuggage() <= 5);
+        assertTrue(passenger.getNumLuggage() >= 0 && passenger.getNumLuggage() <= 5);
+        marksAwarded = marksAwarded + 1;
     }
 
     @Test
-    void testCabinClass() {
+    public void testCabinClassAttribute() {
         // Test the cabinClass attribute type
         assertTrue(passenger.getCabinClass() == 'F' || passenger.getCabinClass() == 'B' ||
                    passenger.getCabinClass() == 'P' || passenger.getCabinClass() == 'E');
+        marksAwarded = marksAwarded + 1;
     }
 
     @Test
-    void testAssignRandomCabinClass() {
+    public void testPassengerCreation()
+    {
+        assertEquals(passportNumber, passenger.getPassportNumber());
+        assertEquals(firstName, passenger.getFirstName());
+        assertEquals(lastName, passenger.getLastName());
+        assertEquals(flightNo, passenger.getFlightNo());
+        marksAwarded = marksAwarded + 2;
+
+        assertTrue(passenger.getNumLuggage() >= 0 && passenger.getNumLuggage() <= 5);
+        marksAwarded = marksAwarded + 2;
+
+        assertNotNull(passenger.getCabinClass());
+        marksAwarded = marksAwarded + 1;
+    }
+
+    @Test
+    public void testAssignRandomCabinClass() {
         // Test the assignRandomCabinClass method
         passenger.assignRandomCabinClass();
         assertTrue(passenger.getCabinClass() == 'F' || passenger.getCabinClass() == 'B' ||
                    passenger.getCabinClass() == 'P' || passenger.getCabinClass() == 'E');
+        marksAwarded = marksAwarded + 2;
     }
 
     @Test
-    void testToString() {
+    public void testToString() {
         // Test the toString method
-        String expectedString = "PP NO. TA890789 NAME: J Bean NUMLUGGAGE: " + passenger.getNumLuggage() +
-                                " CLASS: " + passenger.getCabinClass();
+        String expectedString = " PP NO. TA890789 NAME: J.BEAN NUMLUGGAGE: " + passenger.getNumLuggage() + " CLASS: " + passenger.getCabinClass();
         assertEquals(expectedString, passenger.toString());
+        marksAwarded = marksAwarded + 3;
     }
+
 }
